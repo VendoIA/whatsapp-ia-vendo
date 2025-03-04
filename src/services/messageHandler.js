3db7a7a79b11fb3b7308df0d5dc69240282028de
valtierijd <valtieri.jd@gmail.com>
2025-03-04 17:04:42 -0500

    se modificos package.json y se cambio de nodemon.js a nodemon.json, tambien se cambio lo de controller y problemas de denominacion

--- src/services/messageHandler.js
+++ src/services/messageHandler.js
@@ -3,11 +3,27 @@ import whatsappService from './whatsappService.js';
 class MessageHandler {
   async handleIncomingMessage(message) {
     if (message?.type === 'text') {
-      const response = `Echo: ${message.text.body}`;
-      await whatsappService.sendMessage(message.from, response, message.id);
+      const IncomingMessage = message.text.body.toLowerCase().trim();
+
+      if(this.isGreetings(IncomingMessage)) {
+        await this.sendWelcomeMessage(message.from, message.id);
+      }  else {
+        const response = `Echo: ${message.text.body}`;
+        await whatsappService.sendMessage(message.from, response, message.id);
+      }
       await whatsappService.markAsRead(message.id);
     }
   }
+
+  isGreetings(message) {
+    const grettings = ['hi', 'hello', 'hey', 'hola', 'ola', 'buenos dias', 'buenas tardes', 'buenas noches', 'precio', 'cotizacion', 'informacion', 'ayuda', 'soporte', 'contacto', 'saludo', 'saludos', 'buen dia', 'buenas tardes', 'buenas noches', 'buenas', 'buen', 'tarde', 'noche', 'dia', 'tardes', 'noches', 'dias'];
+    return grettings.includes(message);
+  }
+async sendWelcomeMessage(to, messageId) {
+  const sendWelcomeMessage = `Hola! Soy un bot de WhatsApp. ¿En qué puedo ayudarte?`;
+  await whatsappService.sendMessage(to, sendWelcomeMessage, messageId);
+  }
+
 }
 
 export default new MessageHandler();
